c05ed72ab50e9df1538e18a73b9cf5d2604f8c72
shubhampawar17 <sp1180253@gmail.com>
2024-11-20 18:51:56 +0530

    Add project files.

---
+++ PasswordHashing/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PasswordHashing.Data;
+using PasswordHashing.DTOs;
+using PasswordHashing.Models;
+
+namespace PasswordHashing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly PasswordContext _passwordContext;
+        public UserController(PasswordContext passwordContext)
+        {
+            _passwordContext = passwordContext;
+        }
+        [HttpPost("create_user")]
+        public IActionResult CreateUser(UserDto userDto)
+        {
+            if (string.IsNullOrEmpty(userDto.UserName) || string.IsNullOrEmpty(userDto.Password))
+            {
+                return BadRequest("User and Password are required");
+            }
+            var passwordHash = BCrypt.Net.BCrypt.HashPassword(userDto.Password);
+            var user = new User
+            {
+                UserName = userDto.UserName,
+                PasswordHash = passwordHash,
+                Email = userDto.Email
+            };
+
+            _passwordContext.Add(user);
+            _passwordContext.SaveChanges();
+
+            return Ok("user created");
+        }
+        [HttpPost("Login")]
+        public IActionResult Login(LoginDto loginDto)
+        {
+            if (string.IsNullOrEmpty(loginDto.UserName) && string.IsNullOrEmpty(loginDto.Password))
+            {
+                return BadRequest("Username and password are required");
+            }
+            var user = _passwordContext.Users.FirstOrDefault(u => u.UserName == loginDto.UserName);
+            if (user == null || !BCrypt.Net.BCrypt.Verify(loginDto.Password, user.PasswordHash))
+            {
+                return BadRequest("Invalid Credentials");
+            }
+            return Ok("Login Successfully");
+        }
+    }
+}
---
+++ PasswordHashing/Data/PasswordContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordHashing.Models;
+
+namespace PasswordHashing.Data
+{
+    public class PasswordContext : DbContext
+    {
+        public PasswordContext(DbContextOptions<PasswordContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
